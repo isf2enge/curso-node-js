b1ad66d757868606e53611f53d3b81897e104eff
isf2enge <isf2@discente.ifpe.edu.br>
2022-11-12 06:38:25 -0300

    quinta-parte

--- nodi.js
+++ nodi.js
@@ -1,12 +1,16 @@
-const htm = require('express')
-const html = htm()
+const novo = require('sequelize')
+const novo2 =  new novo('quartos','root','89781258b',{ //criação do banco de dados
+    host:'localhost',
+    dialect : 'mysql'
+});
 
-html.get('/',function(req,res){
+const quarta = novo2.define('quartas',{
 
-    res.sendFile( __dirname + '/arquivo/arquivo.html')
-})
+    nome: {
+        type: novo.STRING
+    },
+    idade: {
+        type: novo.INTEGER}
+}) ;
 
-html.listen(8081,function(){
-
-    console.log('Programa rodando!')
-})
+quarta.sync({force:true}) //só usa uma vez,se não ele re-usa de novo.
